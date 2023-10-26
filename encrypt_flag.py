b1df4cd5489fdd3e0b087b643e47ff7f422b4ccc
cArjun <arjun.lalith@capitalone.com>
2023-10-25 22:35:46 -0400

    initial commit

---
+++ encrypt_flag.py
@@ -0,0 +1,8 @@
+key = ""
+flag = open('flag.txt', 'r').read()
+enc_flag = ""
+for i in range(len(flag)):
+    enc_flag += chr(ord(flag[i]) ^ ord(key[(i % len(key))]) % 128)
+
+open('enc_flag.txt','w').write(enc_flag)
+
